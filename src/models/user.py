2847ddcc3b8be6136348a93fd00bc53692694cd5
Heipiao <lsl8315@gmail.com>
2025-01-14 00:27:58 +0800

    [bugfix] 增加hash 密码长度

--- src/models/user.py
+++ src/models/user.py
@@ -11,7 +11,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(512), nullable=False)
     phone_number = db.Column(db.String(20), nullable=True)
     apple_id = db.Column(db.String(128), unique=True, nullable=True)
     tiktok_id = db.Column(db.String(128), unique=True, nullable=True)
